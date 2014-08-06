bc986952208b61b4364ddce7b0c2ba84755e0973
Ethan Welborn <welborn.ethan@gmail.com>
2014-08-05 21:48:29 -0600

    starting user module

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-'use strict'
-
-angular.module 'needle', [
-]
+angular.module('needle', [
+	'needle.user'
+]);
---
+++ js/user/user.js
@@ -0,0 +1,5 @@
+angular.module('needle.user', [])
+
+.controller('UserCtrl', ['$scope', function($scope) {
+
+}])
