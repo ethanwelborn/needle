a4106d0947d9109ddcb4b44fd57ff781f634bcc7
Ethan Welborn <welborn.ethan@gmail.com>
2014-08-05 22:01:16 -0600

    using user controller on home page

--- js/user/user.js
+++ js/user/user.js
@@ -1,5 +1,14 @@
 angular.module('needle.user', [])
 
-.controller('UserCtrl', ['$scope', function($scope) {
-
+.controller('UserCtrl', ['$scope', 'userService', function($scope, userService) {
+	$scope.users = [
+		{
+			name: 'Guy',
+			email: 'guy@place.com'
+		}
+	];
 }])
+
+.factory('userService', function() {
+
+});
