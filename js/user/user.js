51b659e83dbf9affa4f5f1999309a67d0625545c
Ethan Welborn <welborn.ethan@gmail.com>
2014-08-05 22:44:25 -0600

    button for spawning the modal

--- js/user/user.js
+++ js/user/user.js
@@ -1,12 +1,16 @@
 angular.module('needle.user', [])
 
-.controller('UserCtrl', ['$scope', 'userService', function($scope, userService) {
+.controller('UserCtrl', ['$scope', 'userService', 'modalFactory', function($scope, userService, modalFactory) {
 	$scope.users = [
 		{
 			name: 'Guy',
 			email: 'guy@place.com'
 		}
 	];
+
+	$scope.showModal = function showModal() {
+		modalFactory.activate;
+	};
 
 	$scope.addUser = function addUser() {
 		console.log('hello');
