e75f789e2ff46bd69435a2938a1ea1a287f529aa
Ethan Welborn <welborn.ethan@gmail.com>
2014-08-05 22:04:19 -0600

    setting up a simple ngroute so we dont hardcode the controller on index

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
 angular.module('needle', [
+	'ngRoute',
 	'needle.user'
-]);
+])
+
+.config(function($routeProvider, $locationProvider) {
+	$routeProvider
+		.when('/' {
+			templateUrl: 'user/index.html',
+			controller: 'UserCtrl'
+		});
+});
